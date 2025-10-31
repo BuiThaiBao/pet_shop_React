06b7bca850d0d1c6faaa8fd9a7a3bc1d91c385fc
BuiThaiBao <117577165+BuiThaiBao@users.noreply.github.com>
2025-10-31 23:26:02 +0700

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "https://serverpetshop.onrender.com",
       changeOrigin: true,
       secure: false,
       ws: false,
